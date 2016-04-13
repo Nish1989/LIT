0ff47d7b6a04d02aa88e188d01bd157da591ccf0
unknown <nishanth.papps@gmail.com>
2016-04-13 22:40:23 +0530

    LIT Source Code

---
+++ webpages/gallary/upload.php
@@ -0,0 +1,18 @@
+<form action="" method="post" enctype="multipart/form-data">
+<p>Pictures:
+<input type="file" name="pictures[]" />
+<input type="file" name="pictures[]" />
+<input type="file" name="pictures[]" />
+<input type="submit" value="Send" />
+</p>
+</form>
+
+<?php
+foreach ($_FILES["pictures"]["error"] as $key => $error) {
+   if ($error == UPLOAD_ERR_OK) {
+       $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+       $name = $_FILES["pictures"]["name"][$key];
+       move_uploaded_file($tmp_name, "uploadedArts/$name");
+   }
+}
+?>
